af77d8f7d7ec2920451951e1ae550999da56cef8
whoaditi <aditirai624@gmail.com>
2025-08-19 23:59:55 +0530

    donation page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import LandingPage from './components/LandingPage';
 import Footer from './components/Footer';
+import DonationPage from './components/DonationPage';
 
 function App() {
   return (
     <div className="App">
-     <LandingPage/>
+     <DonationPage/>
      <Footer/>
     </div>
   );
